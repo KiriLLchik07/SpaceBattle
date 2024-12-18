6843761a81b119e26e48e2a66c8a28ee02a52fcc
KiriLLchik07 <kirill3456777@mail.ru>
2024-12-18 20:19:00 +0600

    Adding IoC for Rotate

---
+++ SpaceBattle_Tests/RotationTest/RegisterIoCDependencyRotateCommandTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+using SpaceBattle_workspace;
+
+namespace SpaceBattle_workspace_Tests;
+
+public class RotateCommandIoCTests
+{
+    public RotateCommandIoCTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+    }
+
+    [Fact]
+    public void Execute_ShouldRegisterRotateCommandDependency()
+    {
+        var mockRotate = new Mock<IRotatingObject>();
+        var mockGameObject = new Mock<IDictionary<string, object>>();
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Adapters.IRotatingObject",
+            (Func<object, IRotatingObject>)(obj =>
+            {
+                return mockRotate.Object;
+            })).Execute();
+
+        new RegisterIoCDependencyRotateCommand().Execute();
+
+        var rotateCommand = IoC.Resolve<SpaceBattle_workspace.ICommand>("Commands.Rotate", mockGameObject.Object);
+        Assert.NotNull(rotateCommand);
+        Assert.IsType<RotateCommand>(rotateCommand);
+    }
+}
